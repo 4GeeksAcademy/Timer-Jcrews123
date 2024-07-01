cd55506b75088610b8e156e7e59cc6a7ae0d4074
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-07-01 20:01:04 +0000

    finished timer portion

---
+++ src/js/component/Clock.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Clock = (props) =>{
+    return(
+        <div className="text-center bg-dark" style={{height:"250px", width:"900px"}}>
+            <Clock />
+    </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect , useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Clock } from "./Clock";
 
 //create your first component
 const Home = () => {
+	const [seconds, setSeconds] = useState(0);
+	useEffect(()=> {
+			const startCounter = setInterval(() => {
+				setSeconds(prevSeconds => prevSeconds + 1);
+			}, 
+			1000);
+		return() => clearInterval(startCounter)} , [])
+	const formatTime = (time) => {
+		return time.toString().padStart(6,'0').split('');
+	}
+	const digits = formatTime(seconds);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center">
+			<div className="text-center bg-dark" style={{height:"250px", width:"900px"}}>
+				<div id="container" className= "d-flex flex-column align-items-center flex-md-row justify-content-between text-center">
+					<div className="bg-secondary"style={{height:"100px", width:"100px"}}>
+					<i className="fa-regular fa-clock bg-secondary fa-5x" style={{height:"100px", width:"100px", color:"white"}}></i>
+					</div>
+						{digits.map((digits,index) =>(
+					<div key = {index} className="bg-secondary"style={{height:"100px", width:"100px"}}>
+						<p>{digits}</p>
+					</div>))}
+					
+				</div>
+			</div>
 		</div>
 	);
+	
 };
 
 export default Home;
